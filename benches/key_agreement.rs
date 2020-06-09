12d93c25ae55da3fb2df6133a38eec8798259aa7
Vladislav Melnik <vladislav.melnik@protonmail.com>
2020-06-09 10:53:04 +0300

    improve bench

--- benches/key_agreement.rs
+++ benches/key_agreement.rs
@@ -1,28 +1,35 @@
-use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
 use criterion_cycles_per_byte::CyclesPerByte;
 
 use pq_newhope::{cpa::Cpa, cca::Cca};
-use generic_array::{GenericArray, sequence::GenericSequence, typenum::U128};
+use generic_array::{GenericArray, sequence::GenericSequence, typenum::{U32, U64, U128}};
 
-fn cpa() {
-    let (pk_a, sk_a) = Cpa::<U128>::generate(&GenericArray::generate(|_| rand::random()));
-    let (ct, key_b) = Cpa::<U128>::encapsulate(&pk_a, &GenericArray::generate(|_| rand::random()));
+fn cpa(g: &GenericArray<u8, U32>, e: &GenericArray<u8, U32>) {
+    let (pk_a, sk_a) = Cpa::<U128>::generate(g);
+    let (ct, key_b) = Cpa::<U128>::encapsulate(&pk_a, e);
     let key_a = Cpa::<U128>::decapsulate(&sk_a, &ct);
-    assert_eq!(key_a, key_b);
+    black_box((key_a, key_b));
 }
 
-fn cca() {
-    let (pk_a, sk_a) = Cca::<U128>::generate(&GenericArray::generate(|_| rand::random()));
-    let (ct, key_b) = Cca::<U128>::encapsulate(&pk_a, &GenericArray::generate(|_| rand::random()));
+fn cca(g: &GenericArray<u8, U64>, e: &GenericArray<u8, U32>) {
+    let (pk_a, sk_a) = Cca::<U128>::generate(g);
+    let (ct, key_b) = Cca::<U128>::encapsulate(&pk_a, e);
     let key_a = Cca::<U128>::decapsulate(&sk_a, &ct);
-    assert_eq!(key_a, key_b);
+    black_box((key_a, key_b));
 }
 
 fn bench(c: &mut Criterion<CyclesPerByte>) {
     let mut group = c.benchmark_group("key_agreement");
 
-    group.bench_function(BenchmarkId::new("cpa", 0), |b| b.iter(|| cpa()));
-    group.bench_function(BenchmarkId::new("cca", 0), |b| b.iter(|| cca()));
+    let (g_cpa, g_cca, e_cpa, e_cca) = (
+        GenericArray::generate(|_| rand::random()),
+        GenericArray::generate(|_| rand::random()),
+        GenericArray::generate(|_| rand::random()),
+        GenericArray::generate(|_| rand::random()),
+    );
+
+    group.bench_function(BenchmarkId::new("cpa", 0), |b| b.iter(|| cpa(&g_cpa, &e_cpa)));
+    group.bench_function(BenchmarkId::new("cca", 0), |b| b.iter(|| cca(&g_cca, &e_cca)));
 
     group.finish()
 }
